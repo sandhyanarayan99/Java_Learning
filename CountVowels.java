b45d641081229f6a6de54d563eae01911b0ff21e
Sandhyanarayan99 <sandhyanarayan9988@gmail.com>
2025-09-15 20:33:15 +0530

    - Day 7: Strings in Java (Vowels, Palindrome, Reverse) ✅

---
+++ CountVowels.java
@@ -0,0 +1,16 @@
+public class CountVowels {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        str = str.toLowerCase();
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+
+        System.out.println("Number of vowels: " + count);
+    }
+}
